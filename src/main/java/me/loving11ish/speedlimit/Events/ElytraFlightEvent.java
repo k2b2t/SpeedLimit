b3d9840e0905d000c3a7cc51ae17cb05e21ae9e9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 23:40:36 +0000

    Completely rewritten the ElytraFlightEvent class and added additional options to config.yml. This is ALPHA and is EXTREMELY EXPERIMENTAL!

--- src/main/java/me/loving11ish/speedlimit/Events/ElytraFlightEvent.java
+++ src/main/java/me/loving11ish/speedlimit/Events/ElytraFlightEvent.java
@@ -3,6 +3,7 @@ package me.loving11ish.speedlimit.Events;
 import me.loving11ish.speedlimit.SpeedLimit;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,13 +12,13 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class ElytraFlightEvent implements Listener {
 
     public static Integer taskID1;
-    private static double vDouble;
+    private static double velocityMulti;
 
     public static void updateElytraVelocity(){
         taskID1 = Bukkit.getScheduler().scheduleSyncDelayedTask(SpeedLimit.getPlugin(), new Runnable() {
             @Override
             public void run() {
-                vDouble = SpeedLimit.getPlugin().getConfig().getDouble("Elytra-speed-limit");
+                velocityMulti = SpeedLimit.getPlugin().getConfig().getDouble("Elytra-speed-limit");
             }
         }, 20);
     }
@@ -25,6 +26,20 @@ public class ElytraFlightEvent implements Listener {
     @EventHandler
     public void onElytraFlight(PlayerMoveEvent event){
         Player player = event.getPlayer();
+        double x = event.getFrom().getX();
+        double y = event.getFrom().getY();
+        double z = event.getFrom().getZ();
+        float yaw = event.getFrom().getYaw();
+        float pitch = event.getFrom().getPitch();
+        if (SpeedLimit.getPlugin().getConfig().getBoolean("Disable-all-elytra-flight")){
+            if (player.isGliding()){
+                Location location = new Location(player.getWorld(), x, y, z, yaw, pitch);
+                player.setGliding(false);
+                player.teleport(location);
+                player.sendMessage(ChatColor.RED + "Elytra's have been disabled by and administrator");
+            }
+            return;
+        }
         if (!(SpeedLimit.getPlugin().getConfig().getBoolean("Enable-elytra-limit"))){
             return;
         }
@@ -36,9 +51,15 @@ public class ElytraFlightEvent implements Listener {
             return;
         }
         if (player.isGliding()){
-            if (Math.abs(event.getFrom().getX() - event.getTo().getX()) > vDouble
-                    ||Math.abs(event.getFrom().getZ() - event.getTo().getZ()) > vDouble){
-                event.setCancelled(true);
+            if (Math.abs(event.getFrom().getX() - event.getTo().getX()) > velocityMulti
+                    ||Math.abs(event.getFrom().getY() - event.getTo().getY()) > velocityMulti
+                    ||Math.abs(event.getFrom().getZ() - event.getTo().getZ()) > velocityMulti){
+                Location oldLocation = new Location(player.getWorld(), x, y, z, yaw, pitch);
+                if (SpeedLimit.getPlugin().getConfig().getBoolean("Cancel-event-completely")){
+                    event.setCancelled(true);
+                }else {
+                    player.teleport(oldLocation);
+                }
                 if (SpeedLimit.getPlugin().getConfig().getBoolean("Send-warning-message")){
                     player.sendMessage(ChatColor.RED + "You are flying too fast! Slow down!");
                 }
